dd144d5e7a8c4b1a35ae17f37d030418d2433c13
Sang Nguyễn <sang20052018@gmail.com>
2025-09-17 12:18:38 +0700

    Cập nhật code

--- WebAPI/Data/AppDbContext.cs
+++ WebAPI/Data/AppDbContext.cs
@@ -24,7 +24,7 @@ namespace WebAPI.Data
         }
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
-        public DbSet<Book_Author> Book_Authors { get; set; }
+        public DbSet<Book_Author> Books_Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
     }
 }
---
+++ WebAPI/Models/DTO/addBookRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using WebAPI.Models.Domain;
+namespace WebAPI.Models.DTO
+{
+    public class AddBookRequestDTO
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string? Genre { get; set; }
+        public string? CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+        //navigational Properties - 
+        public int PublisherId { get; set; }
+        public List<int> AuthorIds { get; set; }
+    }
+}
